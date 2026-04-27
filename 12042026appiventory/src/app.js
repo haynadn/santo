377a13a76b7cb090d83b0ad768b5eba36db57984
adnhyna <adnanhadiyana6@gmail.com>
2026-04-27 11:51:59 +0700

    fix: increase json limit to 50mb for large excel import

--- 12042026appiventory/src/app.js
+++ 12042026appiventory/src/app.js
@@ -24,8 +24,8 @@ app.set('views', path.join(__dirname, '../views'));
 
 // Middleware
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
 app.use(methodOverride('_method'));
 
 // Trust proxy jika di belakang nginx/reverse proxy
